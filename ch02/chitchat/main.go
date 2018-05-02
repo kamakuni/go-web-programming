3fc36665cfd0ec50f44ff3faba7d974b54d512ec
kamakuni <superradio86@gmail.com>
2018-05-02 17:41:30 +0900

    create functions in main file_

--- ch02/chitchat/main.go
+++ ch02/chitchat/main.go
@@ -9,9 +9,19 @@ func main() {
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir("/public"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	/*
+		mux.HandleFunc("/", index)
+		mux.HandleFunc("/err", err)
+		mux.HandleFunc("/login", login)
+		mux.HandleFunc("/logout", logout)
+		mux.HandleFunc("/signup", signup)
+		mux.HandleFunc("/signup_account", aignupAccount)
+		mux.HandleFunc("authenticate", authenticate)
 
-	mux.HandleFunc("/", index)
-
+		mux.HandleFunc("/thread/new", newThread)
+		mux.HandleFunc("/thread/create", createThread)
+		mux.HandleFunc("/thread/post", postThread)
+		mux.HandleFunc("/thread/add", addThread)*/
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: mux,
@@ -20,6 +30,7 @@ func main() {
 	server.ListenAndServe()
 }
 
+/*
 func index(w http.ResponseWriter, r http.Request) {
 	files := []string{"templates/layout.html",
 		"templates/navbar.html",
@@ -31,3 +42,4 @@ func index(w http.ResponseWriter, r http.Request) {
 		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }
+*/
