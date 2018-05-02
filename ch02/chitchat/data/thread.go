48c7b64660513e49942f5aed43ff974a3d080b80
kamakuni <superradio86@gmail.com>
2018-05-02 18:37:13 +0900

    add Threads function within thread.go

--- ch02/chitchat/data/thread.go
+++ ch02/chitchat/data/thread.go
@@ -11,3 +11,19 @@ type Thread struct {
 	UserId    int
 	CreatedAt time.Time
 }
+
+func Threads(threads []Thread, err error) {
+	rows, err := Db.Query("SELECT id, uuid, topic, user_id, created_at FROM threads ORDER BY created_at DESC")
+	defer rows.Close()
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		th := Thread{}
+		if err = rows.Scan(&th.Id, &th.Uuid, &th.Topic, &th.UserId, &th.CreatedAt); err != nil {
+			return
+		}
+		threads = append(threads, th)
+	}
+	return
+}
