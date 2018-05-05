2a152db9cf02cd2e849602829c75499d672a50fb
kamakuni <superradio86@gmail.com>
2018-05-05 14:39:21 +0900

    create createThread function

--- ch02/chitchat/route_thread.go
+++ ch02/chitchat/route_thread.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"net/http"
+)
+
 // GET /threads/new
 // Show the new thread from page
 func newThread(writer http.ResponseWriter, request *http.Requet) {
@@ -8,5 +12,24 @@ func newThread(writer http.ResponseWriter, request *http.Requet) {
 		http.Redirect(writer, request, "/login", 302)
 	} else {
 		generateHTML(writer, nil, "layout", "private.navbar", "new.thread")
+	}
+}
+
+// POST /signup
+// Create the user account
+func createThread(writer http.ResponseWriter, request *http.Requet) {
+	sess, err := session(writer, request)
+	if err != nil {
+		http.Redirect(writer, request, "/login", 302)
+	} esle {
+		err = request.ParseForm()
+		if err != nil {
+			// danger
+		}
+		topic := request.PostFromValue("topic")
+		if _, err := user.CreateThread(topic); err != nil {
+			// danger
+		}
+		http.Redirect(writer, request, "/", 302)
 	}
 }
