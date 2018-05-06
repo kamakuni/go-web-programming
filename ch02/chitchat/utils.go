c7523ac4192a7341ecd9ad73e07dc3591d33aa9d
kamakuni <superradio86@gmail.com>
2018-05-06 20:55:54 +0900

    add

--- ch02/chitchat/utils.go
+++ ch02/chitchat/utils.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"os"
 	"errors"
 	"github.com/kamakuni/go-web-programming/ch02/chitchat/data"
 	"html/template"
 	"net/http"
+	"log"
 )
+
+var logger *log.Logger
+
+func init() {
+	file, err := os.OpenFile("chitchat.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND,0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file", err)
+	}
+	logger = log.New(file, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 // Checks if the user is logged in and has a session, if not err is not nil
 func session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
