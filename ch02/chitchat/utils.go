9aea6140443d6e7b90da5973573781cb5b1fc8b2
kamakuni <superradio86@gmail.com>
2018-05-07 23:33:35 +0900

    add loadConfig function within utils.go

--- ch02/chitchat/utils.go
+++ ch02/chitchat/utils.go
@@ -26,6 +26,19 @@ func init() {
 	logger = log.New(file, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
+func loadConfig() {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalln("Cannot open config file", err)
+	}
+	decoder := json.NewDecoder(file)
+	config = Configuration{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalln("Cannotget configuration from file", err)
+	}
+}
+
 func error_message(writer http.ResponseWriter, request *http.Request, msg string) {
 	url := []string("/err?msg=", msg)
 	http.Redirect(writer, request, strings.Join(url, ""), 302)
